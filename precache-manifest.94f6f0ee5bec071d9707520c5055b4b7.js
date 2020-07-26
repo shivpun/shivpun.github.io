e2d8fd182410f0e38c685a403fb5a9859607ec9c
Punit Tiwari <punittiwari58@gmail.com>
2020-07-26 20:24:29 +0530

    Updates

---
+++ precache-manifest.94f6f0ee5bec071d9707520c5055b4b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71168b21ae0de76d5d5afb5824725370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be3fe4086066d9b6e85",
+    "url": "/static/css/main.c2517890.chunk.css"
+  },
+  {
+    "revision": "350764f4db58b6c7076a",
+    "url": "/static/js/2.436e35c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.436e35c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1943ef4b37b145bf54",
+    "url": "/static/js/3.6c33a1c9.chunk.js"
+  },
+  {
+    "revision": "8be3fe4086066d9b6e85",
+    "url": "/static/js/main.1ead2d11.chunk.js"
+  },
+  {
+    "revision": "36f00b391309994a8f2c",
+    "url": "/static/js/runtime-main.5a3ce216.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
